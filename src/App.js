27f7761c27e8e004f025e91b522ed9ee9aa809cd
Ivan <ivanwong810520@gmail.com>
2023-05-02 15:28:33 +0800

    Created message component with subscribe button
    ReactJS Tutorial - 10 - State
    It shows how state works in class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Greet from "./components/Greet.jsx";
 import Welcome from "./components/Welcome.jsx";
+import Message from "./components/Message.jsx";
 
 function App() {
     return (
         <div className="App">
-            <Greet name="Ivan">
+            <Message />
+            {/* <Greet name="Ivan">
                 <p>This is children props</p>
             </Greet>
-            <Welcome name="Ivan" />
+            <Welcome name="Ivan" /> */}
         </div>
     );
 }
