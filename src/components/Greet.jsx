547f6e6c571feb2a40c7f195f796a13ac1baa266
Ivan <ivanwong810520@gmail.com>
2023-05-02 14:30:56 +0800

    Created Functional Components
    ReactJS Tutorial - 5 - Functional Components

---
+++ src/components/Greet.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/*
+function Greet() {
+    return <h1>Hello Ivan</h1>
+}
+*/
+
+// name export method, need import it with the exact same name, for eg: "import { Greet } from './components/Greet.jsx';"
+/*
+export const Greet = () => <h1>Hello Ivan</h1>
+*/
+
+const Greet = () => <h1>Hello Ivan</h1>
+
+// default export method, ucan import it in app.js with any name, for eg: "import GreetComponent from './components/Greet.jsx';"
+export default Greet;
