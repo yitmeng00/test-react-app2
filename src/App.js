f49dcd70c3fe65ad511af38e75a77a33ee4e7e24
Ivan <ivanwong810520@gmail.com>
2023-05-02 14:36:22 +0800

    Created Class Components
    ReactJS Tutorial - 6 - Class Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Greet from './components/Greet.jsx';
+import Welcome from './components/Welcome.jsx';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
 }
