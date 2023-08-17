b2d9d9d0ce586d9165b3c45a5e6c42bcd8dba391
Ivan <ivanwong810520@gmail.com>
2023-08-17 09:52:51 +0800

    Added codes for "Methods as props" tutorial

---
+++ src/components/ChildComponent.jsx
@@ -0,0 +1,12 @@
+// Methods as props
+import React from "react";
+
+function ChildComponent(props) {
+    return (
+        <div>
+            <button onClick={() => props.greetHandler('child')}>Greet Parent</button>
+        </div>
+    );
+}
+
+export default ChildComponent;
---
+++ src/components/ParentComponent.jsx
@@ -0,0 +1,29 @@
+// Methods as props
+import React, { Component } from "react";
+import ChildComponent from "./ChildComponent";
+
+class ParentComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            parentName: "Parent",
+        };
+
+        this.greetParent = this.greetParent.bind(this);
+    }
+
+    greetParent(childName) {
+        alert(`Hello ${this.state.parentName} from ${childName}`);
+    }
+
+    render() {
+        return (
+            <>
+                <ChildComponent greetHandler={this.greetParent} />
+            </>
+        );
+    }
+}
+
+export default ParentComponent;
